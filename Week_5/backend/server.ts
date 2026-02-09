f2b31feb24f96ce21f8354446156e670b5559ae4
Mehnuma Tabassum Omar <60996419+Misty-2409@users.noreply.github.com>
2026-02-09 09:38:41 -0600

    Create server.ts

---
+++ Week_5/backend/server.ts
@@ -0,0 +1,105 @@
+import express from "express";
+import cors from "cors";
+//WEEK 5 -- Addition
+/* Previously (week 1 - 4)
+    HTML + API came from same server
+    Same origin
+    No CORS needed
+Now (Week 5)
+React: http://localhost:5173
+Backend: http://localhost:3002
+Different origins
+Browser blocks requests unless backend explicitly allows them
+This is browser security, not a bug.*/
+
+
+const app = express();
+// ✅ CORS MUST come BEFORE routes
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
+
+const PORT = 3002;
+
+// --------------------
+// TypeScript type
+// --------------------
+type User = {
+  id: number;
+  name: string;
+};
+
+// --------------------
+// Fake database (in memory)
+// --------------------
+let users: User[] = [
+  { id: 1, name: "Ada" },
+  { id: 2, name: "Alan" }
+];
+
+// --------------------
+// Middleware
+// --------------------
+app.use(express.json());
+app.use(express.static("public"));
+
+
+/*
+CRUD	HTTP task that the node will do
+----	----------------------------
+Create	POST
+Read	GET
+Update	PUT / PATCH
+Delete	DELETE
+
+*/
+// --------------------
+// GET all users
+// --------------------
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+// --------------------
+// ADD user
+// --------------------
+app.post("/users", (req, res) => {
+  const newUser: User = {
+    id: Date.now(),
+    name: req.body.name
+  };
+
+  users.push(newUser);
+  res.json(newUser);
+});
+
+// --------------------
+// UPDATE user
+// --------------------
+app.put("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const user = users.find(u => u.id === id);
+
+  if (!user) {
+    return res.status(404).send("User not found");
+  }
+
+  user.name = req.body.name;
+  res.json(user);
+});
+
+// --------------------
+// DELETE user
+// --------------------
+app.delete("/users/:id", (req, res) => {
+  const id = Number(req.params.id);
+  users = users.filter(u => u.id !== id);
+  res.sendStatus(204);
+});
+
+// --------------------
+// Start server
+// --------------------
+app.listen(PORT, () => {
+  console.log("Server running at http://localhost:" + PORT);
+});
